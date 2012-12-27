d6f15e77e51141e62128eb12ea4a6fa6d9544def
Eric Stein <toba@des.truct.org>
2012-12-26 18:51:28 -0600

    messing around with drawing vectors away from the origin (color the ones on axes r g b for x y z) - trying to figure out how the coordinate system I'm doing is working.

--- stereololography/projection.py
+++ stereololography/projection.py
@@ -13,7 +13,7 @@ class Projector(object) :
 		self.c = c
 		self.t = t
 		# position has to be messed around. It doesn't work quite right sense so far.
-		self.e = (0.0,0.0,-5.0)
+		self.e = (0.0,0.0,2.0)
 
 	# TODO add a parameter for how large a viewport to use, and what units. Add support to svgcuts to use all the units stereololography supports
 	# TODO add an option to automatically home the camera? should it work by translating and scaling the render after it's done, or adjusting the 
@@ -21,7 +21,41 @@ class Projector(object) :
 	def project(self, o) :
 		# TODO don't default the size and units of the layer.
 		layer = svgcuts.Layer(8.0, 6.0, unit="in")
+		
 		self.project_inner(layer, o)
+
+		# TODO move this to example? or parameterize it somehow. Either way, this is important for getting your bearings on how the
+		# coordinate system is laid out.
+		n = 2
+		r = 0.1
+		m = .8
+		for x in range(-n, n + 1) :
+			for y in range(-n, n + 1) :
+				for z in range(-n, n + 1) :
+					if x == 0 and y == 0 and z == 0 :
+						continue
+
+					c = r * m
+					_c = math.pow(math.pow(x, 2.0) + math.pow(y, 2.0) + math.pow(z, 2.0), 0.5)
+					xm = x / _c * c
+					ym = y / _c * c
+					zm = z / _c * c
+					p1 = stl.Point(x * m, y * m, z * m)
+					p2 = stl.Point(x * m + xm, y * m + ym, z * m + zm)
+
+					p1 = self.project_point(p1)
+					p2 = self.project_point(p2)
+
+					# axes colors
+					kw = {}
+					if y == 0 and z == 0 :
+						kw['color'] = 'red'
+					elif x == 0 and z == 0 :
+						kw['color'] = 'green'
+					elif x == 0 and y == 0 :
+						kw['color'] = 'blue'
+
+					layer.add_line(svgcuts.Line(p1, p2, **kw))
 		return layer
 
 	def project_inner(self, l, o) :
